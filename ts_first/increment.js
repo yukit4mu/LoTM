8ad0c018dc9c6e681dcbc89c34b1dfd9a5ee363c
yukit4mu <yua_kitamura@decogn.com>
2024-02-13 21:34:38 +0900

    test react

---
+++ ts_first/increment.js
@@ -0,0 +1,8 @@
+// function increment(num: number) {
+//     return num + 1;
+// }
+// console.log(increment("999"));
+function increment(num) {
+    return num + 1;
+}
+console.log(increment(999));
---
+++ ts_first/increment.ts
@@ -0,0 +1,11 @@
+// function increment(num: number) {
+//     return num + 1;
+// }
+
+// console.log(increment("999"));
+
+function increment(num: number) {
+    return num + 1;
+}
+
+console.log(increment(999));
